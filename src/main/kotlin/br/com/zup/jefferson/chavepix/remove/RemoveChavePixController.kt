5fd02599e1c8c8c515500cb820ea863977299d62
jefferson-bit <jhonsonbabi@hotmail.com>
2021-07-01 13:46:38 -0300

    Removendo chave pix

---
+++ src/main/kotlin/br/com/zup/jefferson/chavepix/remove/RemoveChavePixController.kt
@@ -0,0 +1,39 @@
+package br.com.zup.jefferson.chavepix.remove
+
+import br.com.zup.jefferson.RemoveChavePixRequest
+import br.com.zup.jefferson.RemoveChavePixServiceGrpc
+import br.com.zup.jefferson.utils.ValidUUID
+import br.com.zup.jefferson.utils.ValidaChavePix
+import io.micronaut.core.annotation.Introspected
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
+import io.micronaut.validation.Validated
+import java.util.*
+import javax.inject.Inject
+import javax.validation.Valid
+import javax.validation.constraints.NotBlank
+
+@Validated
+@Controller(value = "/api/v1/clientes/{clienteId}/chave")
+class RemoveChavePixController(@Inject val grpcRemove: RemoveChavePixServiceGrpc.RemoveChavePixServiceBlockingStub) {
+
+    @Delete
+    fun remove(clienteId: UUID, @Valid @Body removePixRequest: RemovePixRequest) : HttpResponse<RemovePixRequest>{
+        val response = removePixRequest.toModelParaGrpc(clienteId)
+        grpcRemove.remove(response)
+        return HttpResponse.noContent()
+    }
+}
+
+@Introspected
+@ValidaChavePix
+data class RemovePixRequest(@field:NotBlank val pixId: String?) {
+    fun toModelParaGrpc(clienteId: UUID): RemoveChavePixRequest? {
+        return RemoveChavePixRequest.newBuilder()
+            .setIdCliente(clienteId.toString())
+            .setPixId(pixId)
+            .build()
+    }
+}
